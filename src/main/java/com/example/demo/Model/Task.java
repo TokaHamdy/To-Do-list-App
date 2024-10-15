544ff56c711b674587a917b8f323623a794ecf90
TokaHamdy <doudyhamdy.th@gmail.com>
2024-10-15 20:03:10 +0300

    Task Entity & set up configuration

---
+++ src/main/java/com/example/demo/Model/Task.java
@@ -0,0 +1,18 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+}
